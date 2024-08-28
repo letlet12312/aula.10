9183526ca3f6c8087821160d60967c2c186cd8dd
letlet12312 <leticia.maria.santos100@gmail.com>
2024-08-28 18:01:16 -0300

    Add files via upload

---
+++ aula11.py
@@ -0,0 +1,30 @@
+#ATIVIDADE 1
+
+# import statistics
+
+# sistema_de_notas=()
+ 
+# notas = [7.5,8.5,9.5,6.5,10.5]
+# media = statistics.mean(notas)
+# print("média", media)
+
+# notas = [7.5,8.5,9.5,6.5,10.5]
+# moda= statistics.median(notas)
+# print("moda",moda)
+
+# notas = [7.5,8.5,9.5,6.5,10.5]
+# desvio_padrao = statistics.stdev(notas)
+# print("desvio_padrao",desvio_padrao)
+
+# notas= [7.5,8.5,9.5,6.5,10.5]
+# menor_nota = min(notas)
+# print("menor nota", menor_nota)
+
+# notas= [7.5,8.5,9.5,6.5,10.5]
+# maior_nota = max(notas)
+# print("maior nota", maior_nota)
+
+
+
+# ATIVIDADE 2
+# import statistics
